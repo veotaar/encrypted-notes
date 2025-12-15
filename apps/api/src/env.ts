a358502dab46e449a46ff8389a423298b0e85d05
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2025-12-15 04:33:05 +0300

    feat(api): add drizzle config

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -5,7 +5,7 @@ const EnvSchema = z.object({
 	DATABASE_URL: z.string(),
 	BETTER_AUTH_SECRET: z.string(),
 	BETTER_AUTH_URL: z.url(),
-	PORT: z.coerce.number().default(4000),
+	PORT: z.coerce.number().default(3000),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
