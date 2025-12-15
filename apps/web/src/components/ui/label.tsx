630dc9fdeb943004d3ecbdca8489d1b23f5c8933
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2025-12-16 01:07:38 +0300

    chore(web): replace radix with base ui

--- apps/web/src/components/ui/label.tsx
+++ apps/web/src/components/ui/label.tsx
@@ -1,15 +1,10 @@
-"use client";
-
-import * as LabelPrimitive from "@radix-ui/react-label";
 import { cn } from "@web/lib/utils";
 import type * as React from "react";
 
-function Label({
-	className,
-	...props
-}: React.ComponentProps<typeof LabelPrimitive.Root>) {
+function Label({ className, ...props }: React.ComponentProps<"label">) {
 	return (
-		<LabelPrimitive.Root
+		// biome-ignore lint/a11y/noLabelWithoutControl: r
+		<label
 			data-slot="label"
 			className={cn(
 				"flex select-none items-center gap-2 font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50",
