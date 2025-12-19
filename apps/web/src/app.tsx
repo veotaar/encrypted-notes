cfc853f4be135016f84e74d51a9c4b5d9d945f2d
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2025-12-19 04:23:56 +0300

    feat(web): add auth context

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,10 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { AuthProvider, useAuth } from "@web/lib/auth-context";
 
 import { routeTree } from "./routeTree.gen";
+
+const queryClient = new QueryClient();
 
 // Create a new router instance
 const router = createRouter({
 	routeTree,
+	context: {
+		// biome-ignore lint/style/noNonNullAssertion: e
+		auth: undefined!,
+		queryClient,
+	},
 	defaultPreload: "intent",
 	scrollRestoration: true,
 	defaultViewTransition: false,
@@ -18,9 +27,16 @@ declare module "@tanstack/react-router" {
 }
 
 const InnerApp = () => {
-	return <RouterProvider router={router} />;
+	const auth = useAuth();
+	return <RouterProvider router={router} context={{ auth }} />;
 };
 
-const App = () => <InnerApp />;
+const App = () => (
+	<QueryClientProvider client={queryClient}>
+		<AuthProvider>
+			<InnerApp />
+		</AuthProvider>
+	</QueryClientProvider>
+);
 
 export default App;
--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,7 +1,17 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import { formDevtoolsPlugin } from "@tanstack/react-form-devtools";
+import type { QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import type { AuthContextType } from "@web/lib/auth-context";
 
-export const Route = createRootRoute({
+type RootRouteContext = {
+	queryClient: QueryClient;
+	auth: AuthContextType;
+};
+
+export const Route = createRootRouteWithContext<RootRouteContext>()({
 	component: RootComponent,
 });
 
@@ -10,6 +20,8 @@ function RootComponent() {
 		<>
 			<Outlet />
 			<TanStackRouterDevtools />
+			<ReactQueryDevtools buttonPosition="top-right" position="top" />
+			<TanStackDevtools plugins={[formDevtoolsPlugin()]} />
 		</>
 	);
 }
