ea4753b23d8f5cb73d85abae6eb47ded511d40e6
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2025-12-22 00:07:29 +0300

    feat(api): create insert and update schemas for notes with drizzle-zod
    zod schemas to be used in request and response validation

---
+++ apps/api/src/modules/note/model.ts
@@ -0,0 +1,29 @@
+import { table } from "@api/db/model";
+import {
+	createInsertSchema,
+	createSelectSchema,
+	createUpdateSchema,
+} from "drizzle-zod";
+import type { z } from "zod";
+
+export namespace NoteModel {
+	export const note = {
+		insert: createInsertSchema(table.note).omit({
+			id: true,
+			createdAt: true,
+			updatedAt: true,
+			deletedAt: true,
+		}),
+		select: createSelectSchema(table.note),
+		update: createUpdateSchema(table.note).omit({
+			id: true,
+			createdAt: true,
+			updatedAt: true,
+			deletedAt: true,
+		}),
+	};
+
+	export type NoteInsert = z.infer<typeof note.insert>;
+	export type NoteSelect = z.infer<typeof note.select>;
+	export type NoteUpdate = z.infer<typeof note.update>;
+}
